b12e601f5074b1ba843fedda9739a9504676205f
Nicholas Stanford <stanfordnas@gmail.com>
2025-10-06 22:14:21 -0600

    Initial work on layout. Header and logo complete.

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+    <template>
+      <div class="message-model">
+        <p>{{ message }}</p>
+      </div>
+    </template>
+
+    <script setup>
+    import { ref } from 'vue';
+    const message = ref("Hello from Vue.js in Laravel!");
+    </script>
+
+    <style scoped>
+    .message-model {
+      /* Your styles here */
+    }
+    </style>
+    
---
+++ resources/views/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Invoices | Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap" rel="stylesheet">
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+
+</head>
+
+<body>
+    <header>
+        <app-logo>
+            <app-logo-wrapper>
+                <img src="{{ asset('icons/logo.svg') }}">
+                </img>
+            </app-logo-wrapper>
+        </app-logo>
+    </header>
+</body>
+
+</html>
